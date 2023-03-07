21d34eb54e31cfbc349a85d3933b2bf758c39718
MrPurple666 <antoniosacramento666usa@gmail.com>
2023-03-07 18:44:19 +0000

    changes for t2

--- app/src/main/java/emu/skyline/utils/Colors.kt
+++ app/src/main/java/emu/skyline/utils/Colors.kt
@@ -9,15 +9,15 @@ import android.graphics.Color
 
 enum class SwitchColors(val color : Int) {
     GRAY(Color.GRAY),
-    BLACK(Color.rgb(0, 0, 0)),
-    WHITE(Color.rgb(255, 255, 255)),
-    C1(Color.argb(255,105,180)),
-    C2(Color.argb(128, 112, 203)),
-    C3(Color.argb(252, 236, 82)),
-    C4(Color.argb(93, 46, 140)),
-    C5(Color.argb(46, 196, 182)),
-    C6(Color.argb(0, 117, 162)),
-    C7(Color.argb(235, 50, 95));
+    BLACK(Color.rgb(180, 0, 0, 0)),
+    WHITE(Color.rgb(180, 255, 255, 255)),
+    C1(Color.argb(180, 255,105,180)),
+    C2(Color.argb(180, 128, 112, 203)),
+    C3(Color.argb(180, 252, 236, 82)),
+    C4(Color.argb(180, 93, 46, 140)),
+    C5(Color.argb(180, 46, 196, 182)),
+    C6(Color.argb(180, 0, 117, 162)),
+    C7(Color.argb(180, 235, 50, 95));
 
     companion object {
         val colors get() = SwitchColors.values().map { clr -> clr.color }
